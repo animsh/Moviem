f4d630239af06d18ac5315fb8c7b323419ecdd63
animsh <animsh.more@gmail.com>
2021-02-06 22:45:46 +0530

    changelog: 1.added app logo 2.added splash screen

---
+++ app/src/main/java/com/animsh/moviem/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.animsh.moviem;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
+    }
+}
