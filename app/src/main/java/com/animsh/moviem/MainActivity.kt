a7ace8f416b1aedb815f7d3f7ae5d7a598496088
animsh <animsh.more@gmail.com>
2021-02-24 19:24:27 +0530

    changelog: added base fragments

--- app/src/main/java/com/animsh/moviem/MainActivity.kt
+++ app/src/main/java/com/animsh/moviem/MainActivity.kt
@@ -2,10 +2,34 @@ package com.animsh.moviem
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.fragment)
+        val appBarConfiguration: AppBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.comingSoonFragment,
+                R.id.moreFragment
+            )
+        )
+
+        bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
